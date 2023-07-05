54f55cdeae4c51f652ccbcd825a0e33854e1cf1f
Carlos de la Flor <carlitos.delaflor@gmail.com>
2023-07-05 14:11:25 -0500

    Remove default cypress test.

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,10 +1,10 @@
 describe('sample render test', () => {
   beforeEach(() => {
-    cy.visit('/')
+    //cy.visit('/')
   })
 
   it('displays the title text', () => {
-    cy.get('h2')
-    .contains('Essentials for a cold winter');
+    /*cy.get('h2')
+    .contains('Essentials for a cold winter');*/
   })
 }) 
