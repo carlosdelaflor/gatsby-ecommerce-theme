75a84f15de78366b066f9c001f9140b7b8a6c6aa
Carlos de la Flor <carlitos.delaflor@gmail.com>
2023-08-08 16:28:12 -0500

    Add viewer page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,19 @@
 // https://stackoverflow.com/questions/63124432/how-do-i-configure-mini-css-extract-plugin-in-gatsby
 exports.onCreateWebpackConfig = (helper) => {
   const { stage, actions, getConfig } = helper;
+  const config = getConfig();
+
+  // Added when pdf-react was added.
+  if (stage === 'develop-html') {
+    //pdf-react fix
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    actions.replaceWebpackConfig(config);
+  }
+
+  //Inicial load from github repo fork
   if (stage === 'develop' || stage === 'build-javascript') {
-    const config = getConfig();
+    //const config = getConfig();
     const miniCssExtractPlugin = config.plugins.find(
       (plugin) => plugin.constructor.name === 'MiniCssExtractPlugin'
     );
